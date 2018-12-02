4c24f0d0017b38449d4301c876d05e8a549f1ae3
Namsons Davis <davis.namsons@tieto.com>
2018-12-02 11:49:40 +0200

    small improvement in day 1 ex. 1 code

--- day1/chronal_calibration.rb
+++ day1/chronal_calibration.rb
@@ -3,8 +3,7 @@ input = File.open('./freqs.in').read
 frequency = 0
 
 input.each_line do |line|
-  line_number = line.scan(/[-+]\d+/).first
-  frequency += line_number.to_i
+  frequency += line.to_i
 end
 
 puts frequency
